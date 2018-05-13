52f8cacc7f36be96eeb37510871bc87c4ec60e89
Miguel Figaredo <mfigaredo@gmail.com>
2018-05-13 01:37:28 -0500

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+
+const articles = document.getElementById('articles');
+
+if(articles) {
+   articles.addEventListener('click', (e) => {
+      // alert(2);
+      if(e.target.className=='btn btn-danger delete-article') {
+         // alert(3);
+         if(confirm('Are you sure?')) {
+            const id = e.target.getAttribute('data-id');
+            // alert(id);
+            fetch(`/article/delete/${id}`,{
+               method: 'DELETE'
+            }).then(res => window.location.reload())
+            ;
+         }
+      }
+   });
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+}
